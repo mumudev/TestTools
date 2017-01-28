fc251466d14825e4b226a1e548c207e7f452414b
mumudev <mumudev@163.com>
2017-01-28 12:18:36 +0800

    Remove $.datatable and test gulp-git.

--- app/widgets/dataTable.vue
+++ app/widgets/dataTable.vue
@@ -27,48 +27,6 @@
             });
         },
         mounted() {
-            this.datatable = $('#datatable').DataTable({
-                buttons: this.buttons,
-                columnDefs: this.columnDefs,
-                ajax: {
-                    url: this.url
-                },
-                jQueryUI: false,
-                "aaSorting": [
-                    [1, "desc"]
-                ],
-                language: {
-                    search: "查找",
-                    zeroRecords: "没有匹配的数据",
-                    infoEmpty: "没有符合条件的记录",
-                    info: "第_PAGE_页/共 _PAGES_页",
-                    lengthMenu: "_MENU_ 条/页",
-                    infoFiltered: "(从 _MAX_条记录中过滤)",
-                    paginate: {
-                        "first": "首页 ",
-                        "last": "末页",
-                        "next": "下一页",
-                        "previous": "上一页"
-                    }
-                },
-                select: {
-                    style: 'os',
-                    className: 'active'
-                },
-                paging: true,
-                autoWidth: true,
-                bLengthChange: true, //改变每页显示数据数量
-                pageLength: 10,
-                processing: true, //是否显示加载中提示
-                responsive: true
-            });
-            var self = this;
-            $(".btn-set-all").on("click", function (e) {
-                self.datatable.page.len(-1).draw();
-            });
-            this.$watch('url', function (newVal, oldVal) {
-                console.log('newVal');//这里再感受下值拿到了没
-            });
         },
         components: {
         },
